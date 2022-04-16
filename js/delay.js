8d2e94e257a134a8cffc66435839babb3438eae3
P-CH <69605689+P-CH@users.noreply.github.com>
2022-04-16 16:00:53 +0200

    Update delay.js

--- js/delay.js
+++ js/delay.js
@@ -1,6 +1,6 @@
 let delay = time => new Promise(res => setTimeout(res, time));
 
-document.getElementById("").onclick = async () => {
+document.body.onclick = async () => {
     await delay(400);
-    console.log("sent 400ms after being called");
+    console.log("sent 400ms after the body was clicked");
 }
